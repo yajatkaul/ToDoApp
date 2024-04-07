524b59238a96020b543edba3ba9eada5667e4475
yajatkaul <mc.crafter1973@gmail.com>
2024-04-07 13:55:23 +0530

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function addTask(){
 
 taskbox.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
-        e.target.classlist.toggle("checked");
+        e.target.classList.toggle("checked");
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
